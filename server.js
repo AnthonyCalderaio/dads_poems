80a6d5eaf843d06326c74b388f74e9df98ba0340
Anthony <anthonycalderaio@gmail.com>
2022-09-11 17:39:24 -0400

    postbuild

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/dads-poems'));
+app.use(express.static(__dirname + "/dist/dads-poems"));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/dads-poems/index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/dist/dads-poems/index.html");
 });
 
 // Start the app by listening on the default Heroku port
